b3592c1eef771d0c584b2154ba7c8df81911f3d5
val <val.antonini@gmail.com>
2019-08-15 07:35:58 +1000

    test simplification

--- test/unit/fastmapper.spec.ts
+++ test/unit/fastmapper.spec.ts
@@ -1,29 +1,49 @@
 import fastmapper from "@/index";
 import { expect } from "chai";
-import { Destination, Source } from "./Objects";
 
 describe("Mapping primitive", () => {
-  let source!: Source;
-  let destination!: Destination;
-
-  beforeEach(() => {
-    source = new Source();
-    destination = new Destination();
-
-    fastmapper(destination, source);
-  });
 
   it("should map a string correctly", () => {
-    expect(destination.aString).to.equal("source");
+    const source = {
+      prop: "source",
+    };
+
+    const destination = {
+      prop: "dest",
+    };
+
+    fastmapper(destination, source);
+
+    expect(destination.prop).to.equal("source");
   });
 
   it("should not create a property on the destination", () => {
-    expect(destination).to.not.have.property("onlyOnSource");
+    const source = {
+      prop: "source",
+    };
+
+    const destination = {};
+
+    fastmapper(destination, source);
+
+    expect(source).to.have.property("prop");
+    expect(destination).to.not.have.property("prop");
   });
 
   it("should not map when type does not match", () => {
-    expect(destination.overwriteNumberWithString).to.equal(2);
-    expect(typeof destination.overwriteNumberWithString).to.equal("number");
+    const source = {
+      prop: "one",
+    };
+
+    const destination = {
+      prop: 2,
+    };
+
+    fastmapper(destination, source);
+
+    expect(destination.prop).to.equal(2);
+    expect(typeof destination.prop).to.equal("number");
+    expect(typeof source.prop).to.equal("string");
   });
 
 });
